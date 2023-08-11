2bd0e2027fe25b2c22d9dffee83d734e93ab4784
Maulana Sodiqin <maulansdqn@gmail.com>
2023-08-11 13:12:57 +0700

    improve: add eslint integration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response, NextFunction } from "express";
+import express, { Express, Request, Response } from "express";
 import { bookRouter, userRouter } from "@/routers";
 
 type TRootResponse = {
@@ -10,7 +10,7 @@ export const app: Express = express();
 app.use("/api", userRouter)
 app.use("/api", bookRouter)
 
-app.use("/", (_req: Request, res: Response<TRootResponse>, _next: NextFunction): void => {
+app.use("/", (_req: Request, res: Response<TRootResponse>): void => {
   res.json({ message: "Test" });
 });
 
